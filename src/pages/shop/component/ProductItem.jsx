2026c6a606d31204e3278dfe53ae23c0d151c77c
IDS\dan.tran <ttndan.99@gmail.com>
2021-06-10 18:02:28 +0700

    fix text type

--- src/pages/shop/component/ProductItem.jsx
+++ src/pages/shop/component/ProductItem.jsx
@@ -1,94 +1,121 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { currency } from '../../../utils';
-import { Skeleton } from '@material-ui/lab';
+import React from "react";
+import { useSelector } from "react-redux";
+import { currency } from "../../../utils";
+import { Skeleton } from "@material-ui/lab";
 
-export function ProductItem({ name, images, price, rating_average, discount_rate, slug, thumbnail_url }) {
-	let image1 = images?.[0]?.medium_url;
-	let image2 = images?.[0]?.medium_url;
+export function ProductItem({
+  name,
+  images,
+  price,
+  rating_average,
+  discount_rate,
+  slug,
+  thumbnail_url,
+}) {
+  let image1 = images?.[0]?.medium_url;
+  let image2 = images?.[0]?.medium_url;
 
-	let { loading} = useSelector((state) => state.product);
-	return (
-		<div className="col-6 col-md-4">
-			{/* Card */}
-			<div className="card mb-7">
-				{/* Badge */}
-				<div className="badge badge-white card-badge card-badge-left text-uppercase">New</div>
-				{/* Image */}
-				<div className="card-img">
-					{/* Image */}
-					{loading ? (
-						<Skeleton variant="rect" width={252} height={252} />
-					) : (
-						<a className="card-img-hover" href="product.html">
-							<img className="card-img-top card-img-back" src={thumbnail_url} alt="..." />
-							<img className="card-img-top card-img-front" src={thumbnail_url} alt="..." />
-						</a>
-					)}
-					{/* Actions */}
-					<div className="card-actions">
-						<span className="card-action">
-							<button
-								className="btn btn-xs btn-circle btn-white-primary"
-								data-toggle="modal"
-								data-target="#modalProduct"
-							>
-								<i className="fe fe-eye" />
-							</button>
-						</span>
-						<span className="card-action">
-							<button className="btn btn-xs btn-circle btn-white-primary" data-toggle="button">
-								<i className="fe fe-shopping-cart" />
-							</button>
-						</span>
-						<span className="card-action">
-							<button className="btn btn-xs btn-circle btn-white-primary" data-toggle="button">
-								<i className="fe fe-heart" />
-							</button>
-						</span>
-					</div>
-				</div>
-				{/* Body */}
-				<div className="card-body px-0">
-					{/* Title */}
-					<div className="font-weight-bold">
-						{loading ? (
-							<Skeleton variant="text" />
-						) : (
-							<a className="text-body" href="product.html">
-								{name}
-							</a>
-						)}
-					</div>
+  let { loading } = useSelector((state) => state.product);
+  return (
+    <div className="col-6 col-md-4">
+      {/* Card */}
+      <div className="card mb-7">
+        {/* Badge */}
+        <div className="badge badge-white card-badge card-badge-left text-uppercase">
+          New
+        </div>
+        {/* Image */}
+        <div className="card-img">
+          {/* Image */}
+          {loading ? (
+            <Skeleton variant="rect" width={252} height={252} />
+          ) : (
+            <a className="card-img-hover" href="product.html">
+              <img
+                className="card-img-top card-img-back"
+                src={thumbnail_url}
+                alt="..."
+              />
+              <img
+                className="card-img-top card-img-front"
+                src={thumbnail_url}
+                alt="..."
+              />
+            </a>
+          )}
+          {/* Actions */}
+          <div className="card-actions">
+            <span className="card-action">
+              <button
+                className="btn btn-xs btn-circle btn-white-primary"
+                data-toggle="modal"
+                data-target="#modalProduct"
+              >
+                <i className="fe fe-eye" />
+              </button>
+            </span>
+            <span className="card-action">
+              <button
+                className="btn btn-xs btn-circle btn-white-primary"
+                data-toggle="button"
+              >
+                <i className="fe fe-shopping-cart" />
+              </button>
+            </span>
+            <span className="card-action">
+              <button
+                className="btn btn-xs btn-circle btn-white-primary"
+                data-toggle="button"
+              >
+                <i className="fe fe-heart" />
+              </button>
+            </span>
+          </div>
+        </div>
+        {/* Body */}
+        <div className="card-body px-0">
+          {/* Title */}
+          <div className="font-weight-bold">
+            {loading ? (
+              <Skeleton variant="text" />
+            ) : (
+              <a className="text-body" href="product.html">
+                {name}
+              </a>
+            )}
+          </div>
 
-					{loading ? (
-						<Skeleton variant="text" />
-					) : (
-						<div class="rating font-size-sm text-dark" data-value={rating_average}>
-							<div class="rating-item">
-								<i class="fas fa-star"></i>
-							</div>
-							<div class="rating-item">
-								<i class="fas fa-star"></i>
-							</div>
-							<div class="rating-item">
-								<i class="fas fa-star"></i>
-							</div>
-							<div class="rating-item">
-								<i class="fas fa-star"></i>
-							</div>
-							<div class="rating-item">
-								<i class="fas fa-star"></i>
-							</div>
-						</div>
-					)}
-					{loading ? (
-						<Skeleton variant="rect" />
-					) : (
-						<div className="font-weight-bold text-muted">{currency(price)}</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+          {loading ? (
+            <Skeleton variant="text" />
+          ) : (
+            <div
+              class="rating font-size-sm text-dark"
+              data-value={rating_average}
+            >
+              <div class="rating-item">
+                <i class="fas fa-star"></i>
+              </div>
+              <div class="rating-item">
+                <i class="fas fa-star"></i>
+              </div>
+              <div class="rating-item">
+                <i class="fas fa-star"></i>
+              </div>
+              <div class="rating-item">
+                <i class="fas fa-star"></i>
+              </div>
+              <div class="rating-item">
+                <i class="fas fa-star"></i>
+              </div>
+            </div>
+          )}
+          {loading ? (
+            <Skeleton variant="rect" />
+          ) : (
+            <div className="font-weight-bold text-muted">{currency(price)}</div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/redux/reducer/authReducer.jsx
+++ src/redux/reducer/authReducer.jsx
@@ -1,98 +1,105 @@
-import { LOGIN, LOGIN_ERROR, LOGOUT, REGiSTER, RESGISTER_ERROR, UPDATE_INFO } from './../type';
+import {
+  LOGIN,
+  LOGIN_ERROR,
+  LOGOUT,
+  REGiSTER,
+  REGISTER_ERROR,
+  UPDATE_INFO,
+} from "./../type";
 const initialState = {
-	// login: false,
-	login: JSON.parse(localStorage.getItem('login')) || false,
-	data: JSON.parse(localStorage.getItem('data')) || {},
-	register: {},
-	loginErr: '',
-	registerErr: '',
+  // login: false,
+  login: JSON.parse(localStorage.getItem("login")) || false,
+  data: JSON.parse(localStorage.getItem("data")) || {},
+  register: {},
+  loginErr: "",
+  registerErr: "",
 };
 
 export default function AuthReducer(state = initialState, action) {
-	switch (action.type) {
-		case LOGIN: {
-			localStorage.setItem('login', true);
-			localStorage.setItem('data', JSON.stringify(action.payload));
+  switch (action.type) {
+    case LOGIN: {
+      localStorage.setItem("login", true);
+      localStorage.setItem("data", JSON.stringify(action.payload));
 
-			return {
-				...state,
-				login: true,
-				data: action.payload,
-			};
-		}
-		case LOGOUT: {
-			localStorage.removeItem('login');
-			localStorage.removeItem('data');
-			return {
-				...state,
-				login: false,
-				data: null,
-			};
-		}
-		case REGiSTER: {
-			localStorage.setItem('data', JSON.stringify(action.payload));
+      return {
+        ...state,
+        login: true,
+        data: action.payload,
+      };
+    }
+    case LOGOUT: {
+      localStorage.removeItem("login");
+      localStorage.removeItem("data");
+      return {
+        ...state,
+        login: false,
+        data: null,
+      };
+    }
+    case REGiSTER: {
+      localStorage.setItem("data", JSON.stringify(action.payload));
 
-			return {
-				...state,
-				login: true,
-				data: action.payload,
-			};
-		}
-		case LOGIN_ERROR: {
-			return {
-				...state,
-				loginErr: action.payload,
-			};
-		}
-		case RESGISTER_ERROR: {
-			return {
-				...state,
-				registerErr: action.payload,
-				loginErr: '',
-			};
-		}
-		case UPDATE_INFO: {
-			return {
-				...state,
-				login: true,
-				data: action.payload,
-			};
-		}
-		// case LOGOUT: {
-		// 	localStorage.setItem('login', JSON.stringify(false));
-		// 	localStorage.setItem('data', JSON.stringify(null));
-		// 	localStorage.setItem('home', JSON.stringify(null));
-		// 	return {
-		// 		...state,
-		// 		login: false,
-		// 		data: null,
-		// 	};
-		// }
+      return {
+        ...state,
+        login: true,
+        data: action.payload,
+      };
+    }
+    case LOGIN_ERROR: {
+      return {
+        ...state,
+        loginErr: action.payload,
+      };
+    }
+    case REGISTER_ERROR: {
+      return {
+        ...state,
+        registerErr: action.payload,
+        loginErr: "",
+      };
+    }
+    case UPDATE_INFO: {
+      return {
+        ...state,
+        login: true,
+        data: action.payload,
+      };
+    }
+    // case LOGOUT: {
+    // 	localStorage.setItem('login', JSON.stringify(false));
+    // 	localStorage.setItem('data', JSON.stringify(null));
+    // 	localStorage.setItem('home', JSON.stringify(null));
+    // 	return {
+    // 		...state,
+    // 		login: false,
+    // 		data: null,
+    // 	};
+    // }
 
-		// case UPDATE_INFO: {
-		// 	localStorage.setItem('data', JSON.stringify(action.payload));
-		// 	return {
-		// 		...state,
-		// 		data: action.payload,
-		// 	};
-		// }
-		// case COURSE_REGISTER: {
-		// 	return {
-		// 		...state,
-		// 		successCourse: action.payload,
-		// 	};
-		// }
-		// case HOME: {
-		// 	// localStorage.setItem('home', JSON.stringify(action.payload));
-		// 	return {
-		// 		...state,
-		// 		home: action.payload,
-		// 	};
-		// }
+    // case UPDATE_INFO: {
+    // 	localStorage.setItem('data', JSON.stringify(action.payload));
+    // 	return {
+    // 		...state,
+    // 		data: action.payload,
+    // 	};
+    // }
+    // case COURSE_REGISTER: {
+    // 	return {
+    // 		...state,
+    // 		successCourse: action.payload,
+    // 	};
+    // }
+    // case HOME: {
+    // 	// localStorage.setItem('home', JSON.stringify(action.payload));
+    // 	return {
+    // 		...state,
+    // 		home: action.payload,
+    // 	};
+    // }
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 
-	return state;
+  return state;
 }
